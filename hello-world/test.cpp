913c611db3e5b64916291fdab5a8822484a70ff7
Pedro Feltrin <pedrofeltrin45@gmail.com>
2021-11-11 20:44:20 -0300

    create hello world example with glfw

--- hello-world/test.cpp
+++ hello-world/test.cpp
@@ -3,12 +3,26 @@
 void displayMe(void)
 {
     glClear(GL_COLOR_BUFFER_BIT);
+
     glBegin(GL_POLYGON);
     glVertex3f(0.5, 0.0, 0.5);
     glVertex3f(0.5, 0.0, 0.0);
     glVertex3f(0.0, 0.5, 0.0);
     glVertex3f(0.0, 0.0, 0.5);
     glEnd();
+
+    glBegin(GL_LINES);
+    glColor3f(1.0, 0.0, 0.0);
+    glVertex3i(0, 1, 0);
+    glVertex3i(0, -1, 0);
+    glEnd();
+
+    glBegin(GL_LINES);
+    glColor3f(0.0, 0.0, 1.0);
+    glVertex3i(1, 0, 0);
+    glVertex3i(-1, 0, 0);
+    glEnd();
+
     glFlush();
 }
 
